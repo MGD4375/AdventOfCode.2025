96ee5536dca8c4e2236b8068aa81b1605143b5f8
Morgan G. Davies <MGD4375@Gmail.com>
2025-12-15 22:04:24 +0000

    Performance shenanigans

--- AdventOfCode.2025/Day10/Day10.cs
+++ AdventOfCode.2025/Day10/Day10.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 
 namespace AdventOfCode._2025.Day1;
@@ -50,6 +51,8 @@ public static class Day10
                 return (indicatorLights, buttonsInput: buttons);
             });
 
+        var sw = new Stopwatch();
+        sw.Start();
         var foo = machines.Select(machine =>
         {
             var currentState = new BitArray(machine.indicatorLights.Length);
@@ -57,16 +60,37 @@ public static class Day10
         });
 
         Console.WriteLine(foo.Sum(it => it.stepCount));
+        sw.Stop();
+
+        // Performance with Hashset.Any(it=> it isEqualTo)
+        // Elapsed=00:00:00.7730786
+        // Elapsed=00:00:00.8031957
+        // Elapsed=00:00:00.7872786
+
+        // Performance with ToKey
+        // Elapsed=00:00:00.2306710
+        // Elapsed=00:00:00.2283748
+        // Elapsed=00:00:00.2568954
+
+        // Removing IsEqualsTo altogether for key checks
+        // Elapsed=00:00:00.2136613
+        // Elapsed=00:00:00.1959746
+        // Elapsed=00:00:00.2577227
+        Console.WriteLine("Elapsed={0}", sw.Elapsed);
     }
 
     private static (bool, int stepCount) Bfs(BitArray currentState,
         (BitArray indicatorLights, IEnumerable<BitArray> buttons) machine)
     {
-        var visitedStates = new HashSet<BitArray>();
+        var visitedStates = new HashSet<string>();
+        visitedStates.Add(currentState.ToKey());
 
-        var queue =
-            new Queue<(BitArray currentState, int
-                numberOfSteps)>();
+        var targetState = machine.indicatorLights.ToKey();
+
+        var queue = new Queue<(
+            BitArray currentState,
+            int numberOfSteps
+            )>();
 
         queue.Enqueue((currentState, numberOfSteps: 0));
 
@@ -76,17 +100,19 @@ public static class Day10
             foreach (var button in machine.buttons)
             {
                 var newState = ((BitArray)bitArray.Clone()).Xor(button);
-                if (machine.indicatorLights.IsEqualTo(newState))
+                var key = newState.ToKey();
+
+                if (targetState == key)
                 {
                     return (true, stepCount: stepCount + 1);
                 }
 
-                if (visitedStates.Any(s => s.IsEqualTo(newState)))
+                if (visitedStates.Contains(key))
                 {
                     continue;
                 }
 
-                visitedStates.Add(newState);
+                visitedStates.Add(key);
                 queue.Enqueue((newState, stepCount + 1));
             }
         }
@@ -97,14 +123,5 @@ public static class Day10
 
 public static class BitArrayExtensions
 {
-    public static bool IsEqualTo(this BitArray array1, BitArray array2)
-    {
-        for (var i = 0; i < array1.Length; i++)
-        {
-            if (array1[i] != array2[i])
-                return false;
-        }
-
-        return true;
-    }
+    public static string ToKey(this BitArray ba) => string.Join("", ba.Cast<bool>().Select(b => b ? '1' : '0'));
 }
